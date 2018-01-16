792740fa278ff095ebb6a74290e70a17eb958d22
jbpease <jbpease1@gmail.com>
2018-01-15 20:34:49 -0500

    added mixcore, fasblastalign update

--- fasblastalign.py
+++ fasblastalign.py
@@ -9,7 +9,7 @@ James B. Pease
 import os
 import sys
 import argparse
-from itertools import groupby
+from mixcore import fasta_iter, BlastSet, complement
 
 _LICENSE = """
 http://www.github.org/jbpease/mixtape
@@ -32,191 +32,21 @@ along with MixTAPE.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 
-class BlastHit(object):
-    """Blast hit record"""
-    def __init__(self, queryseq=None, subjectseq=None, pident=None,
-                 length=None, mismatches=None, gaps=None,
-                 query_start=None, query_end=None, subject_start=None,
-                 subject_end=None, evalue=None, bitscore=None):
-        self.queryseq = queryseq
-        self.subjectseq = subjectseq
-        self.pident = float(pident) / 100
-        self.length = int(length)
-        self.mismatches = int(mismatches)
-        self.gaps = int(gaps)
-        self.query_start = int(query_start)
-        self.query_end = int(query_end)
-        self.subject_start = int(subject_start)
-        self.subject_end = int(subject_end)
-        self.evalue = float(evalue)
-        self.bitscore = float(bitscore)
-
-    def orientation(self):
-        if ((self.query_start > self.query_end
-             and self.subject_end > self.subject_start) or
-            (self.query_end > self.query_start and
-             self.subject_start > self.subject_end)):
-            return -1
-        return 1
-
-
-class BlastSet(object):
-    """Set of blast hits"""
-    def __init__(self, hits=None):
-        self.hits = [] if hits is None else hits
-
-    def __len__(self):
-        return len(self.hits)
-
-    def __getitem__(self, k):
-        return self.hits[k]
-
-    def __iter__(self):
-        return iter(self.hits)
-
-    def add_tabular(self, arr, ncol=12):
-        """Reads a standard 12-column blast input as list"""
-        xstart = 0 if ncol == 12 else 2
-        self.hits.append(BlastHit(
-            queryseq=arr[0],
-            subjectseq=arr[1],
-            pident=arr[2 + xstart],
-            length=arr[3 + xstart],
-            mismatches=arr[4 + xstart],
-            gaps=arr[5 + xstart],
-            query_start=arr[6 + xstart],
-            query_end=arr[7 + xstart],
-            subject_start=arr[8 + xstart],
-            subject_end=arr[9 + xstart],
-            evalue=arr[10 + xstart],
-            bitscore=arr[11 + xstart]))
-        return ''
-
-    def parse_blast(self, blastfile, ncol=12):
-        """Parse 12-column tabular BLAST file"""
-        with open(blastfile, 'r') as blastfile:
-            for line in blastfile:
-                if line[0] == '#':
-                    continue
-                if len(line) < 10:
-                    continue
-                self.add_tabular(line.rstrip().split(), ncol=ncol)
-        return ''
-
-    def remove_low_pident(self, cutoff, verbose=True):
-        """Remove hits with low_pident"""
-        if cutoff >= 0 and cutoff <= 1:
-            remlist = []
-            for i in range(len(self.hits)):
-                if self.hits[i].pident < cutoff:
-                    remlist.append(i)
-        remlist.sort(reverse=True)
-        for i in remlist:
-            if verbose:
-                print("{} removed due to low pid").format(
-                    self.hits[i].subjectseq)
-            self.hits.pop(i)
-        return ''
-
-    def remove_superceded(self, verbose=False):
-        """Remove hits where the another
-           sequence completely contains the region"""
-        remlist = []
-        for i in range(len(self.hits) - 1):
-            if i in remlist:
-                continue
-            for j in range(i + 1, len(self.hits)):
-                if overlap_range([self.hits[i].query_start,
-                                  self.hits[i].query_end],
-                                 [self.hits[j].query_start,
-                                  self.hits[j].query_end]) == 2:
-                    remlist.append(i)
-                    break
-                if overlap_range([self.hits[j].query_start,
-                                  self.hits[j].query_end],
-                                 [self.hits[i].query_start,
-                                  self.hits[i].query_end]) == 2:
-                    remlist.append(j)
-        remlist.sort(reverse=True)
-        for i in remlist:
-            if verbose:
-                print("{} superceded".format(self.hits[i].subjectseq))
-            self.hits.pop(i)
-        return ''
-
-    def n_subjectseq(self):
-        """Return number of unique database sequences"""
-        return len(set([hit.subjectseq for hit in self.hits]))
-
-    def get_subjectseqs(self):
-        """Returns a list of database sequence names"""
-        retlist = list(set([hit.subjectseq for hit in self.hits]))
-        retlist.sort()
-        return retlist
-
-    def get_hits(self, subject='', query=''):
-        """Search for specific hits by database name or query name"""
-        retlist = []
-        for hit in self.hits:
-            if ((not subject or subject in hit.subjectseq)
-                    and (not query or query in hit.queryseq)):
-                retlist.append(hit)
-        return retlist
-
-
-def overlap_range(range0, range1, inclusive=True):
-    """
-    Tests whether two ranges of integers overlap,
-    -2 = range0 inside range1
-    -1 = the end of range1 overlaps the start of range0
-    0 = no overlap
-    1 = the end of range0 overlaps the start of range1
-    2 = range1 inside range0
-    """
-    range0 = [min(range0), max(range0)]
-    range1 = [min(range1), max(range1)]
-    if inclusive:
-        range0 = [range0[0] - 1, range0[1] + 1]
-        range1 = [range1[0] - 1, range1[1] + 1]
-    if min(range0) > min(range1) and min(range0) < max(range1):
-        return 2 if max(range0) < max(range1) else -1
-    elif max(range0) > min(range1) and max(range0) < max(range1):
-        return -2 if min(range0) > min(range1) else 1
-    return 0
-
-
-def fasta_iter(fasta_name):
-    """
-        given a fasta file. yield tuples of header, sequence
-        Adapted from https://github.com/brentp
-    """
-    filehandler = open(fasta_name, 'r')
-    faiter = (x[1] for x in groupby(filehandler, lambda line: line[0] == ">"))
-    for header in faiter:
-        header = next(header)[1:].strip()
-        seq = "".join(s.strip() for s in next(faiter))
-        yield header, seq
-
-
-def complement(seq):
-    return seq[::-1].lower().replace(
-        'a', 'T').replace('t', 'A').replace(
-            'c', 'G').replace('g', 'C')
-
-
 def generate_argparser():
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         epilog=_LICENSE)
-    parser.add_argument('-f', "--fasta", type=os.path.abspath,
-                        help="input FASTA file")
-    parser.add_argument('-b', "--blast", type=os.path.abspath,
+    parser.add_argument("--query", type=os.path.abspath,
+                        help="input of query sequence FASTA file")
+    parser.add_argument("--db", type=os.path.abspath,
+                        help="input of db sequences file")
+    parser.add_argument("--blast", type=os.path.abspath,
                         help="input BLAST tabular tab-delimted file")
-    parser.add_argument('-o', '--out', type=os.path.abspath,
+    parser.add_argument('--out', type=os.path.abspath,
                         default=sys.stdout, help="output FASTA file")
-    parser.add_argument('-n', '--ncol', type=int,
-                        default=12, help="number of BLAST columns")
+    parser.add_argument('--ncol', type=int,
+                        default=14, help="number of BLAST columns")
     return parser
 
 
@@ -225,37 +55,76 @@ def main(arguments=None):
     parser = generate_argparser()
     args = parser.parse_args(args=arguments)
     seqs = {}
-    for hdr, seq in fasta_iter(args.fasta):
+    for hdr, seq in fasta_iter(args.db):
+        hdr = hdr.split()[0]
         if ":" in hdr:
             hdr = hdr[:hdr.find(":")]
         seqs[hdr] = seq
+    for hdr, seq in fasta_iter(args.query):
+        hdr = hdr.split()[0]
+        if ":" in hdr:
+            hdr = hdr[:hdr.find(":")]
+        seqs[hdr] = seq
+        query_hdr = hdr[:]
     newseqs = []
     blastdata = BlastSet()
     blastdata.parse_blast(args.blast, ncol=args.ncol)
     seqnames = set([])
+    min_coord = 0
     for blasthit in blastdata:
         if blasthit.queryseq not in seqnames:
             seqnames.update([blasthit.queryseq])
-            newseqs.append((-20000, blasthit.queryseq,
+            newseqs.append((0, blasthit.queryseq,
                             seqs[blasthit.queryseq]))
         if blasthit.subjectseq not in seqnames:
             xseq = seqs[blasthit.subjectseq][:]
             orientation = blasthit.orientation()
-            if orientation == 1:
-                xseq = ("-" * (blasthit.query_start - 1)) + xseq
-                newseqs.append((blasthit.query_start, blasthit.subjectseq,
-                                xseq))
-                seqnames.update([blasthit.subjectseq])
-            elif orientation == -1:
-                xseq = ("-" * (blasthit.query_start - 1)) + complement(xseq)
-                newseqs.append((blasthit.query_start, blasthit.subjectseq,
-                               xseq))
-                seqnames.update([blasthit.subjectseq])
-    maxlen = max(len(x[2]) for x in newseqs)
+            if orientation == -1:
+                xseq = complement(xseq)
+                start_coord = (blasthit.query_start - len(xseq) +
+                               blasthit.subject_start - 1)
+                prefix = "-"
+            else:
+                prefix = "+"
+                start_coord = blasthit.query_start - (
+                    blasthit.subject_start)
+            newseqs.append((start_coord, prefix + blasthit.subjectseq,
+                           xseq))
+            seqnames.update([blasthit.subjectseq])
+            if start_coord < min_coord:
+                min_coord = 0 + start_coord
+    max_coord = max(x[0] + len(x[2]) for x in newseqs)
+    consensus = {}
     with open(args.out, 'w') as outfile:
-        for _, hdr, seq in sorted(newseqs):
-            outfile.write(">{}\n{}{}\n".format(hdr, seq, '-'*(maxlen -
-                                                              len(seq))))
+        for coord, hdr, seq in sorted(newseqs):
+            outfile.write(">{}\n{}{}{}\n".format(
+                hdr,
+                '-' * (coord - min_coord),
+                seq,
+                '-' * (max_coord - coord - len(seq))))
+            if hdr == query_hdr:
+                continue
+            for i in range(len(seq)):
+                xcoord = i + coord - min_coord
+                if xcoord not in consensus:
+                    consensus[xcoord] = {}
+                consensus[xcoord][seq[i]] = consensus[xcoord].get(seq[i], 0) + 1
+        conseq = []
+        print(consensus)
+        for i in range(max(consensus) + 1):
+            if i not in consensus:
+                conseq.append("-")
+            else:
+                print(list(sorted((v, k) for k, v in
+                                consensus[i].items() if k != '-')))
+                conseq.append(
+                    list(sorted((v, k) for k, v in
+                                consensus[i].items() if k != '-'))[-1][1])
+
+
+
+        outfile.write(">CONSENSUS\n{}{}".format(''.join(conseq),
+                                                "-"*(max_coord - len(conseq))))
     return ''
 
 
---
+++ mixcore.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+
+from itertools import groupby
+
+
+def complement(seq):
+    return seq[::-1].lower().replace(
+        'a', 'T').replace('t', 'A').replace(
+            'c', 'G').replace('g', 'C')
+
+
+def fasta_iter(fasta_name):
+    """
+        given a fasta file. yield tuples of header, sequence
+        Adapted from https://github.com/brentp
+    """
+    filehandler = open(fasta_name, 'r')
+    faiter = (x[1] for x in groupby(filehandler, lambda line: line[0] == ">"))
+    for header in faiter:
+        header = next(header)[1:].strip()
+        seq = "".join(s.strip() for s in next(faiter))
+        yield header, seq
+
+
+def overlap_range(range0, range1, inclusive=True):
+    """
+    Tests whether two ranges of integers overlap,
+    -2 = range0 inside range1
+    -1 = the end of range1 overlaps the start of range0
+    0 = no overlap
+    1 = the end of range0 overlaps the start of range1
+    2 = range1 inside range0
+    """
+    range0 = [min(range0), max(range0)]
+    range1 = [min(range1), max(range1)]
+    if inclusive:
+        range0 = [range0[0] - 1, range0[1] + 1]
+        range1 = [range1[0] - 1, range1[1] + 1]
+    if min(range0) > min(range1) and min(range0) < max(range1):
+        return 2 if max(range0) < max(range1) else -1
+    elif max(range0) > min(range1) and max(range0) < max(range1):
+        return -2 if min(range0) > min(range1) else 1
+    return 0
+
+
+class BlastHit(object):
+    """Blast hit record"""
+    def __init__(self, queryseq=None, subjectseq=None, pident=None,
+                 length=None, mismatches=None, gaps=None,
+                 query_start=None, query_end=None, subject_start=None,
+                 subject_end=None, evalue=None, bitscore=None):
+        self.queryseq = queryseq
+        self.subjectseq = subjectseq
+        self.pident = float(pident) / 100
+        self.length = int(length)
+        self.mismatches = int(mismatches)
+        self.gaps = int(gaps)
+        self.query_start = int(query_start)
+        self.query_end = int(query_end)
+        self.subject_start = int(subject_start)
+        self.subject_end = int(subject_end)
+        self.evalue = float(evalue)
+        self.bitscore = float(bitscore)
+
+    def orientation(self):
+        if ((self.query_start > self.query_end
+             and self.subject_end > self.subject_start) or
+            (self.query_end > self.query_start and
+             self.subject_start > self.subject_end)):
+            return -1
+        return 1
+
+
+class BlastSet(object):
+    """Set of blast hits"""
+    def __init__(self, hits=None):
+        self.hits = [] if hits is None else hits
+
+    def __len__(self):
+        return len(self.hits)
+
+    def __getitem__(self, k):
+        return self.hits[k]
+
+    def __iter__(self):
+        return iter(self.hits)
+
+    def add_tabular(self, arr, ncol=12):
+        """Reads a standard 12-column blast input as list"""
+        xstart = 0 if ncol == 12 else 2
+        self.hits.append(BlastHit(
+            queryseq=arr[0],
+            subjectseq=arr[1],
+            pident=arr[2 + xstart],
+            length=arr[3 + xstart],
+            mismatches=arr[4 + xstart],
+            gaps=arr[5 + xstart],
+            query_start=arr[6 + xstart],
+            query_end=arr[7 + xstart],
+            subject_start=arr[8 + xstart],
+            subject_end=arr[9 + xstart],
+            evalue=arr[10 + xstart],
+            bitscore=arr[11 + xstart]))
+        return ''
+
+    def parse_blast(self, blastfile, ncol=12):
+        """Parse 12-column tabular BLAST file"""
+        with open(blastfile, 'r') as blastfile:
+            for line in blastfile:
+                if line[0] == '#':
+                    continue
+                if len(line) < 10:
+                    continue
+                self.add_tabular(line.rstrip().split(), ncol=ncol)
+        return ''
+
+    def remove_low_pident(self, cutoff, verbose=True):
+        """Remove hits with low_pident"""
+        if cutoff >= 0 and cutoff <= 1:
+            remlist = []
+            for i in range(len(self.hits)):
+                if self.hits[i].pident < cutoff:
+                    remlist.append(i)
+        remlist.sort(reverse=True)
+        for i in remlist:
+            if verbose:
+                print("{} removed due to low pid").format(
+                    self.hits[i].subjectseq)
+            self.hits.pop(i)
+        return ''
+
+    def remove_superceded(self, verbose=False):
+        """Remove hits where the another
+           sequence completely contains the region"""
+        remlist = []
+        for i in range(len(self.hits) - 1):
+            if i in remlist:
+                continue
+            for j in range(i + 1, len(self.hits)):
+                if overlap_range([self.hits[i].query_start,
+                                  self.hits[i].query_end],
+                                 [self.hits[j].query_start,
+                                  self.hits[j].query_end]) == 2:
+                    remlist.append(i)
+                    break
+                if overlap_range([self.hits[j].query_start,
+                                  self.hits[j].query_end],
+                                 [self.hits[i].query_start,
+                                  self.hits[i].query_end]) == 2:
+                    remlist.append(j)
+        remlist.sort(reverse=True)
+        for i in remlist:
+            if verbose:
+                print("{} superceded".format(self.hits[i].subjectseq))
+            self.hits.pop(i)
+        return ''
+
+    def n_subjectseq(self):
+        """Return number of unique database sequences"""
+        return len(set([hit.subjectseq for hit in self.hits]))
+
+    def get_subjectseqs(self):
+        """Returns a list of database sequence names"""
+        retlist = list(set([hit.subjectseq for hit in self.hits]))
+        retlist.sort()
+        return retlist
+
+    def get_hits(self, subject='', query=''):
+        """Search for specific hits by database name or query name"""
+        retlist = []
+        for hit in self.hits:
+            if ((not subject or subject in hit.subjectseq)
+                    and (not query or query in hit.queryseq)):
+                retlist.append(hit)
+        return retlist
